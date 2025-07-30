514e41b7552af6aab0bf54751532bbe6ef1605d8
Christian U. <christian@ullihome.de>
2025-07-30 10:36:17 +0200

    add tree and configure some Key bindings

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 #sudo pacman -S python-pynvim
 
+vim.api.nvim_set_keymap("n", "<C-x>", ":qa<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
 vim.g.vimspector_enable_mappings = 'HUMAN'
 
 local ensure_packer = function()
@@ -16,9 +18,41 @@ end
 
 local packer_bootstrap = ensure_packer()
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
 require('packer').startup(function(use)
  use 'wbthomason/packer.nvim'
  use 'puremourning/vimspector'
+
+  -- Syntax-Highlighting und Code-Analyse
+  use "nvim-treesitter/nvim-treesitter"
+  use "neovim/nvim-lspconfig"
+
+  -- Datei- und Projektmanagement
+  use "nvim-telescope/telescope.nvim"
+  use "nvim-tree/nvim-tree.lua"
+
+  -- Statusleiste und Git-Integration
+  use "nvim-lualine/lualine.nvim"
+  use "lewis6991/gitsigns.nvim"
 
  if packer_bootstrap then
  require('packer').sync()
