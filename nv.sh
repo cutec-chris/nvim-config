687bc568877445ccc8bbaa9f2567596012538ee3
Christian U. <christian.ulrich@tcsag.de>
2025-08-07 13:39:53 +0200

    allow to use podman, fix nvim cache not existing

--- nv.sh
+++ nv.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-# Docker-Image-Name definieren
+
+# Docker/Podman-Image-Name definieren
 IMAGE_NAME="my-nvim"
+
 # Pfad zum Script-Verzeichnis ermitteln
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Überprüfen, ob Podman installiert ist
+if command -v podman &> /dev/null; then
+  CONTAINER_CMD="podman"
+else
+  CONTAINER_CMD="docker"
+fi
+
 # Image erstellen
-docker build -t "$IMAGE_NAME" "$SCRIPT_DIR"
-# Aktuellen Ordner als /app mounten
+$CONTAINER_CMD build -t "$IMAGE_NAME" "$SCRIPT_DIR"
+
+# Sicherstellen, dass der Neovim-Cache-Ordner existiert
+NVIM_CACHE_DIR="$HOME/.cache/nvim_cache"
+mkdir -p "$NVIM_CACHE_DIR"
+
 # Aktuellen Ordner als /app mounten und Neovim-Cache mounten
-docker run -it --rm \
- -v "$(pwd):/app" \
- -v "$HOME/.cache/nvim_cache:/root/.local/share/nvim/site/pack" \
- "$IMAGE_NAME" nvim
+$CONTAINER_CMD run -it --rm \
+  -v "$(pwd):/app" \
+  -v "$NVIM_CACHE_DIR:/root/.local/share/nvim/site/pack" \
+  "$IMAGE_NAME" nvim
